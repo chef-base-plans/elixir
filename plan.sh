ca01920ae6c4cbd945880b309adaaa5113667974
Lucas Giancola <lucas.giancola@live.com>
2021-03-17 11:22:02 -0400

    Bumped version 1.10.0 -> 1.11.4

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_origin=core
 pkg_name=elixir
-pkg_version=1.10.0
+pkg_version=1.11.4
 pkg_description="A dynamic, functional language designed for building scalable and maintainable applications. Elixir leverages the Erlang VM, known for running low-latency, distributed and fault-tolerant systems, while also being successfully used in web development and the embedded software domain."
 pkg_upstream_url=http://elixir-lang.org
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('Apache-2.0')
 pkg_source="https://github.com/elixir-lang/elixir/archive/v${pkg_version}.tar.gz"
-pkg_shasum=6f0d35acfcbede5ef7dced3e37f016fd122c2779000ca9dcaf92975b220737b7
+pkg_shasum=9d2019282e5de770483a34c96c10c0c1d3f365b0b80796b5d11c6d11c385683e
 pkg_deps=(
   core/busybox
   core/cacerts
